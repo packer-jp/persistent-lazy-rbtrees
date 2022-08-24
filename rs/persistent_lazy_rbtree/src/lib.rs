49d63a3aca8dabc7e43626af56f929eb2f06d908
packer_jp <tone0805develop@gmail.com>
2022-08-24 17:23:01 +0900

    遅延評価用の構造体

--- rs/persistent_lazy_rbtree/src/lib.rs
+++ rs/persistent_lazy_rbtree/src/lib.rs
@@ -1,7 +1,17 @@
 use std::iter::{DoubleEndedIterator, FromIterator, FusedIterator, IntoIterator};
-use std::ops::{Index, Mul};
+use std::ops::{Add, Index, Range};
 use std::rc::Rc;
 
+pub trait Magma: Sized + Add<Output = Self> + Clone {}
+pub trait Semigroup: Magma {}
+pub trait Monoid: Semigroup {
+    fn zero() -> Self;
+}
+pub trait LazyParam {
+    type Element: Monoid;
+    type Mapping: Monoid;
+    fn apply(mapping: Self::Mapping, element: Self::Element) -> Self::Element;
+}
 #[derive(Clone, Debug, Copy)]
 enum Color {
     Red,
@@ -10,11 +20,13 @@ enum Color {
 use Color::{Black, Red};
 
 #[derive(Debug)]
-enum Node<T> {
+enum Node<T: LazyParam> {
     Leaf {
-        val: T,
+        val: T::Element,
     },
     Tree {
+        val: T::Element,
+        lazy: T::Mapping,
         color: Color,
         rank: usize,
         len: usize,
@@ -23,9 +35,11 @@ enum Node<T> {
     },
 }
 use Node::{Leaf, Tree};
-impl<T: Clone> Node<T> {
+impl<T: LazyParam> Node<T> {
     fn new(color: Color, left: Rc<Node<T>>, right: Rc<Node<T>>) -> Self {
         Tree {
+            val: left.val().clone() + right.val().clone(),
+            lazy: T::Mapping::zero(),
             color,
             rank: left.rank()
                 + match left.color() {
@@ -35,6 +49,12 @@ impl<T: Clone> Node<T> {
             len: left.len() + right.len(),
             left,
             right,
+        }
+    }
+    fn val(&self) -> &T::Element {
+        match self {
+            Leaf { val } => val,
+            Tree { val, .. } => val,
         }
     }
     fn color(&self) -> Color {
@@ -67,7 +87,7 @@ impl<T: Clone> Node<T> {
             Tree { right, .. } => right,
         }
     }
-    fn index(&self, index: usize) -> &T {
+    fn index(&self, index: usize) -> &T::Element {
         match self {
             Leaf { val } => val,
             Tree { left, right, .. } => {
@@ -169,13 +189,11 @@ impl<T: Clone> Node<T> {
         }
     }
 }
-impl<T: Clone + Mul> Node<T> {}
 
-#[derive(Clone, Debug)]
-pub struct PersistentLazyRBTree<T> {
+pub struct PersistentLazyRBTree<T: LazyParam> {
     root: Option<Rc<Node<T>>>,
 }
-impl<T: Clone> PersistentLazyRBTree<T> {
+impl<T: LazyParam> PersistentLazyRBTree<T> {
     fn from(root: Rc<Node<T>>) -> Self {
         Self { root: Some(root) }
     }
@@ -203,7 +221,7 @@ impl<T: Clone> PersistentLazyRBTree<T> {
             (Self::from(left), Self::from(right))
         }
     }
-    pub fn insert(&self, index: usize, val: T) -> Self {
+    pub fn insert(&self, index: usize, val: T::Element) -> Self {
         assert!(index <= self.len());
         let (ref left, ref right) = self.split(index);
         Self::merge(
@@ -217,6 +235,13 @@ impl<T: Clone> PersistentLazyRBTree<T> {
         let (_, ref right) = right.split(1);
         Self::merge(left, right)
     }
+    pub fn fold(&self, range: Range<usize>) -> T::Element {
+        let (left, _) = self.split(range.end);
+        let (_, mid) = left.split(range.start);
+        mid.root
+            .map(|root| root.val().clone())
+            .unwrap_or(T::Element::zero())
+    }
     pub fn iter(&self) -> Iter<T> {
         Iter {
             begin: 0,
@@ -225,21 +250,27 @@ impl<T: Clone> PersistentLazyRBTree<T> {
         }
     }
 }
-
-impl<T: Clone> Index<usize> for PersistentLazyRBTree<T> {
-    type Output = T;
+impl<T: LazyParam> Clone for PersistentLazyRBTree<T> {
+    fn clone(&self) -> Self {
+        Self {
+            root: self.root.clone(),
+        }
+    }
+}
+impl<T: LazyParam> Index<usize> for PersistentLazyRBTree<T> {
+    type Output = T::Element;
     fn index(&self, index: usize) -> &Self::Output {
         assert!(index < self.len());
         self.root.as_ref().unwrap().index(index)
     }
 }
-pub struct Iter<'a, T: 'a> {
+pub struct Iter<'a, T: LazyParam> {
     begin: usize,
     end: usize,
     tree: &'a PersistentLazyRBTree<T>,
 }
-impl<'a, T: Clone> Iterator for Iter<'a, T> {
-    type Item = &'a T;
+impl<'a, T: LazyParam> Iterator for Iter<'a, T> {
+    type Item = &'a T::Element;
     fn next(&mut self) -> Option<Self::Item> {
         if self.begin < self.tree.len() {
             let ret = Some(&self.tree[self.begin]);
@@ -253,12 +284,12 @@ impl<'a, T: Clone> Iterator for Iter<'a, T> {
         (self.tree.len(), Some(self.tree.len()))
     }
 }
-impl<'a, T: Clone> ExactSizeIterator for Iter<'a, T> {
+impl<'a, T: LazyParam> ExactSizeIterator for Iter<'a, T> {
     fn len(&self) -> usize {
         self.tree.len()
     }
 }
-impl<'a, T: Clone> DoubleEndedIterator for Iter<'a, T> {
+impl<'a, T: LazyParam> DoubleEndedIterator for Iter<'a, T> {
     fn next_back(&mut self) -> Option<Self::Item> {
         if self.end > 0 {
             self.end -= 1;
@@ -268,9 +299,9 @@ impl<'a, T: Clone> DoubleEndedIterator for Iter<'a, T> {
         }
     }
 }
-impl<'a, T: Clone> FusedIterator for Iter<'a, T> {}
-impl<T: Clone> FromIterator<T> for PersistentLazyRBTree<T> {
-    fn from_iter<I: IntoIterator<Item = T>>(iter: I) -> Self {
+impl<'a, T: LazyParam> FusedIterator for Iter<'a, T> {}
+impl<T: LazyParam> FromIterator<T::Element> for PersistentLazyRBTree<T> {
+    fn from_iter<I: IntoIterator<Item = T::Element>>(iter: I) -> Self {
         let mut res: Vec<Self> = vec![];
         for item in iter {
             let mut cur = Self::new().insert(0, item);
@@ -294,32 +325,64 @@ impl<T: Clone> FromIterator<T> for PersistentLazyRBTree<T> {
 
 #[cfg(test)]
 mod tests {
-    use crate::PersistentLazyRBTree;
+    use crate::{LazyParam, Magma, Monoid, PersistentLazyRBTree, Semigroup};
     use rand::Rng;
+    use std::ops::Add;
     #[test]
     fn it_works() {
+        #[derive(Clone)]
+        struct SumMonoid(i64);
+        impl Add for SumMonoid {
+            type Output = Self;
+            fn add(self, other: Self) -> Self {
+                Self(self.0 + other.0)
+            }
+        }
+        impl Magma for SumMonoid {}
+        impl Semigroup for SumMonoid {}
+        impl Monoid for SumMonoid {
+            fn zero() -> Self {
+                Self(0)
+            }
+        }
+        struct LazySum;
+        impl LazyParam for LazySum {
+            type Mapping = SumMonoid;
+            type Element = SumMonoid;
+            fn apply(mapping: Self::Mapping, element: Self::Element) -> Self::Element {
+                element
+            }
+        }
         let mut rng = rand::thread_rng();
         let mut vec = Vec::new();
-        let mut rbtree = PersistentLazyRBTree::new();
+        let mut rbtree: PersistentLazyRBTree<LazySum> = PersistentLazyRBTree::new();
         let n = 100000;
+        let m = 100;
         for _ in 0..n {
-            let x: i64 = rng.gen();
+            let x = rng.gen_range(0, m);
             vec.push(x);
-            rbtree = rbtree.insert(rbtree.len(), x);
+            rbtree = rbtree.insert(rbtree.len(), SumMonoid(x));
         }
         let q = 100000;
         for _ in 0..q {
-            let x = rng.gen();
+            let x = rng.gen_range(0, m);
             let i = rng.gen_range(0, vec.len() + 1);
             vec.insert(i, x);
-            rbtree = rbtree.insert(i, x);
+            rbtree = rbtree.insert(i, SumMonoid(x));
 
             let i = rng.gen_range(0, vec.len());
             vec.remove(i);
             rbtree = rbtree.erase(i);
 
             let i = rng.gen_range(0, vec.len());
-            assert_eq!(vec[i], rbtree[i]);
+            assert_eq!(vec[i], rbtree[i].0);
+
+            let l = rng.gen_range(0, vec.len());
+            let r = rng.gen_range(l, vec.len() + 1);
+            assert_eq!(
+                vec.get(l..r).unwrap().iter().fold(0, |sum, i| sum + i),
+                rbtree.fold(l..r).0
+            );
         }
     }
 }
